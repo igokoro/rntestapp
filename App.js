d27a821d6dde24e1eceb87fd4a80f4a64ca9bcd5
Kyle Treman <jkt290@nyu.edu>
2018-06-19 13:11:30 -0400

    basic structure, start login page

---
+++ App.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, TextInput, View, Button } from "react-native";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      storeID: "Enter Your Store ID",
+      storePass: "Enter Your Password"
+    }
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Store ID</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={storeID => this.setState({ storeID })}
+          value={this.state.storeID}
+        />
+        <Text style={styles.text}>Password</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={storePass => this.setState({ storePass })}
+          value={this.state.storePass}
+        />
+        <Button
+          onPress={null}
+          title='Log In'
+          ></Button>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    width: 200,
+  },
+  text: {
+    margin: 13,
+    fontSize: 18
+  }
+});
