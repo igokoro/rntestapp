cb1446cb92748bc626f308bf4dbb0dd43830f4b1
Kyle Treman <jkt290@nyu.edu>
2018-06-22 13:44:41 -0400

    GA reporting

--- components/App.js
+++ components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { createStackNavigator } from "react-navigation";
-import { Platform } from "react-native";
+import { Platform, View } from "react-native";
 import LoginForm from "./LoginForm";
 import OrdersList from "./OrdersList";
 import {
@@ -9,26 +9,51 @@ import {
   GoogleTagManager
 } from "react-native-google-analytics-bridge";
 
-export default class App extends Component {
-  render() {
-
-    let tracker = new GoogleAnalyticsTracker("UA-121230754-2");
-    console.log(tracker)
-    GoogleAnalyticsSettings.setDispatchInterval(30);
-    tracker.trackScreenView("LoginForm");
-
-    return <RootStack />;
+// gets the current screen from navigation state
+function getCurrentRouteName(navigationState) {
+  if (!navigationState) {
+    return null;
   }
+  const route = navigationState.routes[navigationState.index];
+  // dive into nested navigators
+  if (route.routes) {
+    return getCurrentRouteName(route);
+  }
+  return route.routeName;
 }
 
-const RootStack = createStackNavigator(
-  {
-    LoginForm,
-    OrdersList
-  },
-  {
-    initialRouteName: "LoginForm",
-    // removes white space at top of device
-    headerMode: "none"
+const tracker = new GoogleAnalyticsTracker("UA-121230754-2");
+
+class App extends Component {
+  render() {
+    const RootStack = createStackNavigator(
+      {
+        LoginForm: { screen: LoginForm },
+        OrdersList: { screen: OrdersList }
+      },
+      {
+        initialRouteName: "LoginForm",
+        // removes white space at top of device
+        headerMode: "none"
+      }
+    );
+
+    return (
+      <View style={{ flex: 1 }}>
+        <RootStack
+          onNavigationStateChange={(prevState, currentState) => {
+            const currentScreen = getCurrentRouteName(currentState);
+            const prevScreen = getCurrentRouteName(prevState);
+
+            if (prevScreen !== currentScreen) {
+              // the line below uses the Google Analytics tracker
+              // change the tracker here to use other Mobile analytics SDK.
+              tracker.trackScreenView(currentScreen);
+            }
+          }}
+        />
+      </View>
+    );
   }
-);
+}
+export default App;
