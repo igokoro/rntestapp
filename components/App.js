90e5c0b2fddb03ef9c3e3be9a02a54adc58a5a9a
Kyle Treman <jkt290@nyu.edu>
2018-06-22 11:36:29 -0400

    GA bridge not working,  testing a rebuild

--- components/App.js
+++ components/App.js
@@ -3,9 +3,20 @@ import { createStackNavigator } from "react-navigation";
 import { Platform } from "react-native";
 import LoginForm from "./LoginForm";
 import OrdersList from "./OrdersList";
+import {
+  GoogleAnalyticsTracker,
+  GoogleAnalyticsSettings,
+  GoogleTagManager
+} from "react-native-google-analytics-bridge";
 
 export default class App extends Component {
   render() {
+
+    let tracker = new GoogleAnalyticsTracker("UA-121230754-2");
+    console.log(tracker)
+    GoogleAnalyticsSettings.setDispatchInterval(30);
+    tracker.trackScreenView("LoginForm");
+
     return <RootStack />;
   }
 }
