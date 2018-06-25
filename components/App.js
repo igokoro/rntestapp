a4bcccdfb10cdfba63418ccea45f28dae74e60c0
Kyle Treman <jkt290@nyu.edu>
2018-06-25 09:59:02 -0400

    Add comments

--- components/App.js
+++ components/App.js
@@ -45,9 +45,8 @@ class App extends Component {
             const currentScreen = getCurrentRouteName(currentState);
             const prevScreen = getCurrentRouteName(prevState);
 
+            // determine if screen changed 
             if (prevScreen !== currentScreen) {
-              // the line below uses the Google Analytics tracker
-              // change the tracker here to use other Mobile analytics SDK.
               tracker.trackScreenView(currentScreen);
             }
           }}
