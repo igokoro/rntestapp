6679c05f3905561f88048e01a2a4e7e82502d198
Kyle Treman <ktreman@1800flowers.com>
2018-07-02 11:46:09 -0400

    Add status props

--- components/WelcomeSplash.js
+++ components/WelcomeSplash.js
@@ -10,17 +10,21 @@ import styles from "../styles/styles";
 
 class WelcomeSplash extends Component {
   render() {
+
+    const textDisplay = this.props.status || "Loading"
     return (
+      
       <ImageBackground
         source={require("../assets/images/welcomeSplash.jpg")}
         style={styles.splashImage}
         resizeMode={"cover"}
       >
-        <View style={styles.container}>
-          <Text style={[styles.loginText, styles.splashHeader]}>
-            1800Flowers Delivery App
-          </Text>
+        <View
+          style={styles.container}
+        >
+          <Text style={[styles.loginText, styles.splashHeader]}>1800Flowers Delivery App</Text>
           <ActivityIndicator size={100} color="white" />
+          <Text style={[styles.text, styles.loginText]}>{textDisplay}</Text>
         </View>
       </ImageBackground>
     );
