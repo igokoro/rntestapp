284a521769e6025d438bce889ccacbed1d55283d
Kyle Treman <ktreman@1800flowers.com>
2018-07-06 13:51:43 -0400

    Add HeaderText component

---
+++ components/styledComponents/HeaderText.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Text, StyleSheet } from 'react-native';
+
+export default class HeaderText extends Component {
+  render() {
+    return (
+      <Text
+        style={ styles.text }
+        { ...this.props }
+      />
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  text: {
+    fontFamily: "Roboto",
+    fontSize: 22,
+    fontWeight: "500",
+  },
+});
