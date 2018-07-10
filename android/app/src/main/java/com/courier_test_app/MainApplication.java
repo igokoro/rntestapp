9f2d08db26931ef5f1cfa143254973ac3f4e96fc
Kyle Treman <ktreman@1800flowers.com>
2018-07-10 11:17:43 -0400

    Add radar

--- android/app/src/main/java/com/courier_test_app/MainApplication.java
+++ android/app/src/main/java/com/courier_test_app/MainApplication.java
@@ -3,6 +3,7 @@ package com.courier_test_app;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.onradar.react.RNRadarPackage;
 import com.idehub.GoogleAnalyticsBridge.GoogleAnalyticsBridgePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNRadarPackage(),
             new GoogleAnalyticsBridgePackage()
       );
     }
